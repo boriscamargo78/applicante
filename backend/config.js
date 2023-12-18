6f04ff6614409d4f105aa4f9cee10f585c706567
Boris Camargo <44958532+boriscamargo78@users.noreply.github.com>
2023-12-18 17:24:32 -0500

    Update config.js
    Update para remover credenciales

--- backend/config.js
+++ backend/config.js
@@ -4,8 +4,8 @@ puerto: 3000,
 dbMongo: "applicance",
 claveoculta: "8sd7fg6sdf87gs$#%#$d%87f6g",
 tiemposesion: 60000 * 60, // 60 seg por 60 = una hora activa la sesion
-correogmail: "portos1978@gmail.com", // Correo desde donde se va a enviar el correo
-passwordgmail: "jxkxlyytxwgwygrd", // App Password del correo anterior
+correogmail: "", // Correo desde donde se va a enviar el correo
+passwordgmail: "", // App Password del correo anterior
 listablanca: [ "http://localhost:4200"],
 secretsesion:"holamundo",
 namecookie:"Applicante"
